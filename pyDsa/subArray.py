1b56721da664b5cbcabb47ebbfa761899653044d
Manabendu Karfa <rikugithub@gmail.com>
2025-04-10 23:28:31 +0530

    Delete pyDsa directory

--- pyDsa/subArray.py
+++
@@ -1,9 +0,0 @@
-nums = list(map(int, input().split()))
-
-l = len(nums)+1
-
-for i in range(l):
-    for j in range(i,l):
-        for k in range(i,j):
-            print(nums[k], end=' ')
-        print()
